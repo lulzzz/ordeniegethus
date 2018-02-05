700856fe57d09ded484fd45a90fba1ec88bece69
Henning Jensen <henning@arkitektum.no>
2018-02-05 15:34:10 +0100

    Disable self registration of users

--- Arkitektum.Orden/Controllers/AccountController.cs
+++ Arkitektum.Orden/Controllers/AccountController.cs
@@ -205,7 +205,6 @@ namespace Arkitektum.Orden.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public IActionResult Register(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
@@ -213,7 +212,6 @@ namespace Arkitektum.Orden.Controllers
         }
 
         [HttpPost]
-        [AllowAnonymous]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
         {
