333d7b9257cc4d9b50914ea7a7ed2772d9166dcb
Henning Jensen <henning@arkitektum.no>
2018-02-14 16:06:25 +0100

    User must be logged in to use the app

--- Arkitektum.Orden/Controllers/ApplicationsController.cs
+++ Arkitektum.Orden/Controllers/ApplicationsController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Arkitektum.Orden.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = Roles.User)]
     public class ApplicationsController : Controller
     {
         private readonly ApplicationDbContext _context;
