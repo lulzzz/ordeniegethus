2d46b1ddd2580db6a84b3dfc9d308cb4d11e58b9
Henning Jensen <henning@arkitektum.no>
2018-02-16 12:33:48 +0100

    Disable email sending until appsettings issue is fixed

--- Arkitektum.Orden/Services/EmailSender.cs
+++ Arkitektum.Orden/Services/EmailSender.cs
@@ -9,20 +9,20 @@ namespace Arkitektum.Orden.Services
     /// </summary>
     public class EmailSender : IEmailSender
     {
-        private readonly AppSettings _appSettings;
+        //private readonly AppSettings _appSettings;
 
-        public EmailSender(AppSettings appSettings)
-        {
-            _appSettings = appSettings;
-        }
+        //public EmailSender(AppSettings appSettings)
+        //{
+        //    _appSettings = appSettings;
+        //}
 
         public async Task SendEmailAsync(string email, string subject, string message)
         {
-            var client = new SendGridClient(_appSettings.EmailSettings.SendgridApiKey);
-            var from = new EmailAddress(_appSettings.EmailSettings.FromAddress);
-            var to = new EmailAddress(email);
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
-            await client.SendEmailAsync(msg);
+            //var client = new SendGridClient(_appSettings.EmailSettings.SendgridApiKey);
+            //var from = new EmailAddress(_appSettings.EmailSettings.FromAddress);
+            //var to = new EmailAddress(email);
+            //var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
+            //await client.SendEmailAsync(msg);
         }
     }
 }
