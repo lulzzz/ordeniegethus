0683cdd50934948013fdec5076b50eef80c709ad
Anastasiya Bukhala <anastasiya@arkitektum.no>
2018-02-20 08:12:33 +0100

    Datamodell cleanup

--- Arkitektum.Orden/Models/SharedService.cs
+++ Arkitektum.Orden/Models/SharedService.cs
@@ -7,7 +7,6 @@ namespace Arkitektum.Orden.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public int ApplicationId { get; set; }
         public virtual List<ApplicationSharedService> ApplicationSharedServices { get; set; }
 
     }
