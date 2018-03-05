84da96178167bff126e7c983835934adeeb3e45d
Henning Jensen <henning@arkitektum.no>
2018-03-05 20:00:10 +0100

    Add default organizations to DbInitializer

--- Arkitektum.Orden/Data/DbInitializer.cs
+++ Arkitektum.Orden/Data/DbInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Arkitektum.Orden.Models;
 using Microsoft.AspNetCore.Identity;
@@ -19,18 +20,28 @@ namespace Arkitektum.Orden.Data
                 if (!roleManager.RoleExistsAsync(role.ToUpper()).Result)
                     await roleManager.CreateAsync(new IdentityRole {Name = role});
 
-            if (!context.Users.Any())
+            
+            var email = "dev@arkitektum.no";
+            var adminUser = new ApplicationUser
             {
-                var email = "dev@arkitektum.no";
-                var adminUser = new ApplicationUser
-                {
-                    UserName = email,
-                    Email = email
-                };
-                var result = userManager.CreateAsync(adminUser, "Test123").Result;
+                UserName = email,
+                Email = email,
+                FullName = "Administrator"
+            };
+            IdentityResult result = userManager.CreateAsync(adminUser, "Test123").Result;
 
-                if (result.Succeeded) await userManager.AddToRolesAsync(adminUser, Roles.All);
-            }
+            if (result.Succeeded) await userManager.AddToRolesAsync(adminUser, Roles.All);
+
+            var boKommune = new Organization() {Name = "Bø kommune"};
+            context.Add(boKommune);
+            var sauheradKommune = new Organization() {Name = "Sauherad kommune"};
+            context.Add(sauheradKommune);
+            var skienKommune = new Organization() {Name = "Skien kommune"};
+            context.Add(skienKommune);
+            var kongsbergKommune = new Organization() {Name = "Kongsberg kommune"};
+            context.Add(kongsbergKommune);
+
+            context.SaveChanges();
         }
     }
 }
