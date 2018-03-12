279c3f799f7afe78ff8ccf9a2388a2e7e723d616
Henning Jensen <henning@arkitektum.no>
2018-03-12 21:59:48 +0100

    Basic indexing

---
+++ Arkitektum.Orden/Services/Search/ElasticSearchIndexingService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Arkitektum.Orden.Models;
+using Arkitektum.Orden.Services.Search;
+using Nest;
+
+namespace Arkitektum.Orden.Services
+{
+    public interface ISearchIndexingService
+    {
+        Task AddToIndex(Application application);
+        Task AddToIndex(Dataset dataset);
+    }
+
+    public class ElasticSearchIndexingService : ISearchIndexingService
+    {
+        public async Task AddToIndex(Application application)
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+                .DefaultIndex("application");
+            var client = new ElasticClient(settings);
+
+            await client.IndexDocumentAsync(new ApplicationDocument(application));
+        }
+        
+        public async Task AddToIndex(Dataset dataset)
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+                .DefaultIndex("dataset");
+            var client = new ElasticClient(settings);
+
+            await client.IndexDocumentAsync(new DatasetDocument(dataset));
+        }
+    }
+}
---
+++ Arkitektum.Orden/Services/Search/ElasticSearchService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+
+namespace Arkitektum.Orden.Services.Search
+{
+    public interface ISearchService
+    {
+        Task<ISearchResponse<ApplicationDocument>> Search(string query);
+    }
+
+    public class ElasticSearchService : ISearchService
+    {
+        public async Task<ISearchResponse<ApplicationDocument>> Search(string query)
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+                .DefaultIndex("application");
+            var client = new ElasticClient(settings);
+
+            return await client.SearchAsync<ApplicationDocument>(s => s.Query(q => q.MatchAll()));
+        } 
+
+    }
+}
