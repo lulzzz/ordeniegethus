b83a376b40649c0e0f2215af2d8d2356d1d3be6c
Henning Jensen <henning@arkitektum.no>
2018-03-15 21:15:04 +0100

    Visual improvements to search results

--- Arkitektum.Orden/Models/ViewModels/SearchResultViewModel.cs
+++ Arkitektum.Orden/Models/ViewModels/SearchResultViewModel.cs
@@ -1,37 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Arkitektum.Orden.Services.Search;
 
 namespace Arkitektum.Orden.Models.ViewModels
 {
     public class SearchResultViewModel
     {
-        public long NumberOfHits {get; set; }
+        public long NumberOfHits { get; set; }
 
-        public IEnumerable<SearchResultItem> Items { get;set; }
-
+        public IEnumerable<ApplicationDocument> Items { get; set; }
     }
 
 
     public class SearchResultItem
     {
         public int Id { get; set; }
-        public string Name { get;set; }
+        public string Name { get; set; }
 
         public static IEnumerable<SearchResultItem> Map(IReadOnlyCollection<ApplicationDocument> documents)
         {
             var output = new List<SearchResultItem>();
 
             foreach (var doc in documents)
-            {
-                output.Add(new SearchResultItem()
+                output.Add(new SearchResultItem
                 {
                     Id = doc.Id,
                     Name = doc.Name
                 });
-            }
 
             return output;
         }
