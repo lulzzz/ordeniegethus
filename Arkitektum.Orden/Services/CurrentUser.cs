3457424265f173309630a400cdea53a3cea776b1
Henning Jensen <henning@arkitektum.no>
2018-03-22 14:13:07 +0100

    Add ApplicationUser to CurrentUser class

--- Arkitektum.Orden/Services/CurrentUser.cs
+++ Arkitektum.Orden/Services/CurrentUser.cs
@@ -1,24 +1,33 @@
 ﻿using System.Security.Principal;
+using Arkitektum.Orden.Models;
 
 namespace Arkitektum.Orden.Services
 {
     public interface ICurrentUser
     {
         bool IsInRole(string role);
+        IIdentity Identity();
+        string FullName();
     }
 
     public class CurrentUser : ICurrentUser
     {
         private readonly IPrincipal _principal;
+        private readonly ApplicationUser _applicationUser;
 
-        public CurrentUser(IPrincipal principal)
+        public CurrentUser(IPrincipal principal, ApplicationUser applicationUser)
         {
             _principal = principal;
+            _applicationUser = applicationUser;
         }
 
         public bool IsInRole(string role)
         {
             return _principal.IsInRole(role);
         }
+
+        public IIdentity Identity() => _principal.Identity;
+
+        public string FullName() => _applicationUser.FullName;
     }
 }
