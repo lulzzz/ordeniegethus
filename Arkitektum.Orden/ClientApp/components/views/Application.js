7d36e5e89d76aff38de85aeea6ff2a717456c1e7
Benjamin Dehli <benjamin@arkitektum.no>
2018-04-12 18:40:27 +0200

    Add logic and template for application component

--- Arkitektum.Orden/ClientApp/components/views/Application.js
+++ Arkitektum.Orden/ClientApp/components/views/Application.js
@@ -1,11 +1,32 @@
 ﻿/* Components */
 import ResourceLinks from '../partials/ResourceLinks.vue';
+import ApplicationSectors from '../partials/ApplicationSectors.vue';
 
 export default {
     name: 'Application',
-    props: ['apiUrl'],
+    props: ['applicationId'],
+    data() {
+        return {
+            apiUrls: {
+                get: `/Applications/${this.applicationId}`
+            },
+            apiData: null
+        }
+    },
     components: {
-        ResourceLinks
+        ResourceLinks,
+        ApplicationSectors
+    },
+    mounted() {
+        this.getApiData();
+    },
+    methods: {
+        getApiData() {
+            Promise.resolve(this.$root.getApiData(this.apiUrls.get))
+                .then((apiData) => {
+                    this.apiData = apiData;
+                });
+        },
     }
 }
 
