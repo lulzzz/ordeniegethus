4742b198427bc221c0e31a5b2f8590a518265aef
Benjamin Dehli <benjamin@arkitektum.no>
2018-04-18 10:48:11 +0200

    Add event listeners for 'filter select' component

--- Arkitektum.Orden/ClientApp/components/modules/FilterSelect.js
+++ Arkitektum.Orden/ClientApp/components/modules/FilterSelect.js
@@ -1,35 +1,87 @@
 export default {
-    name: 'FilterSelect',
-    props: ['options', 'filterableProperties', 'primaryTextProperty', 'secondaryTextProperty', 'valueProperty'],
-    data() {
-        return {
-            filterResults: [],
-            inputValue: ''
-        }
-    },
-    methods: {
-        updateFilterResult(filterValue) {
-            let filterResults = [];
-            if (filterValue.length){
-                this.options.forEach((option) => {
-                    let optionHasMatch = false;
-                    this.filterableProperties.forEach((filterableProperty) => {
-                        if (option[filterableProperty].toUpperCase().indexOf(filterValue.toUpperCase()) > -1){
-                            optionHasMatch = true;
-                        }
-                    });
-
-                    if (optionHasMatch) {
-                        filterResults.push(option);
-                    }
-                });
-            }
-            this.filterResults = filterResults;
-        },
-        selectFilterResult(filterResult){
-            this.inputValue = filterResult[this.primaryTextProperty];
-            this.$emit('select', filterResult);
-        }
+  name: "FilterSelect",
+  props: [
+    "options",
+    "filterableProperties",
+    "primaryTextProperty",
+    "secondaryTextProperty",
+    "valueProperty",
+    "minSearchLength"
+  ],
+  data() {
+    return {
+      filterResults: [],
+      inputValue: "",
+      active: false
+    };
+  },
+  watch: {
+    options() {
+      if (!this.minSearchLength) {
+        this.updateFilterResult("");
+      }
     }
-}
+  },
+  methods: {
+    updateFilterResult(filterValue) {
+      let filterResults = [];
+      let minSearchLength =
+        this.minSearchLength !== undefined ? this.minSearchLength : 0;
+      if (!filterValue.length) {
+        filterResults = this.options;
+      } else if (filterValue.length >= minSearchLength) {
+        this.options.forEach(option => {
+          let optionHasMatch = false;
+          this.filterableProperties.forEach(filterableProperty => {
+            if (
+              option[filterableProperty]
+                .toUpperCase()
+                .indexOf(filterValue.toUpperCase()) > -1
+            ) {
+              optionHasMatch = true;
+            }
+          });
 
+          if (optionHasMatch) {
+            filterResults.push(option);
+          }
+        });
+      }
+      this.filterResults = filterResults;
+    },
+    selectFilterResult(filterResult) {
+      this.inputValue = filterResult[this.primaryTextProperty];
+      this.$emit("select", filterResult);
+      this.hideResults();
+    },
+    showResults() {
+      this.active = true;
+    },
+    hideResults() {
+      this.active = false;
+    }
+  },
+  directives: {
+    "click-outside": {
+      bind: function(el, binding, vNode) {
+        // Define Handler and cache it on the element
+        const bubble = binding.modifiers.bubble;
+        const handler = e => {
+          if (bubble || (!el.contains(e.target) && el !== e.target)) {
+            binding.value(e);
+          }
+        };
+        el.__vueClickOutside__ = handler;
+
+        // Add Event Listeners
+        document.addEventListener("click", handler);
+      },
+
+      unbind: function(el, binding) {
+        // Remove Event Listeners
+        document.removeEventListener("click", el.__vueClickOutside__);
+        el.__vueClickOutside__ = null;
+      }
+    }
+  }
+};
