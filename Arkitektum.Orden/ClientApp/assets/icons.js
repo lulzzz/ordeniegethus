1464e8b1eb67efc7f214d066aeabc58457d4d2ef
Benjamin Dehli <benjamin@arkitektum.no>
2018-04-19 11:04:24 +0200

    Add icons to navigation bar

--- Arkitektum.Orden/ClientApp/assets/icons.js
+++ Arkitektum.Orden/ClientApp/assets/icons.js
@@ -1,4 +1,4 @@
 ﻿import fontawesome from '@fortawesome/fontawesome';
-import { faUser, faUsers, faEdit, faTimes, faCheck, faTrash, faPlus, faWarehouse } from '@fortawesome/fontawesome-free-solid';
+import { faUser, faUsers, faEdit, faTimes, faCheck, faTrash, faPlus, faWarehouse, faChartLine, faUnlock, faDesktop, faDatabase } from '@fortawesome/fontawesome-free-solid';
 
-fontawesome.library.add([faUser, faUsers, faEdit, faTimes, faCheck, faTrash, faPlus, faWarehouse]);
+fontawesome.library.add([faUser, faUsers, faEdit, faTimes, faCheck, faTrash, faPlus, faWarehouse, faChartLine, faUnlock, faDesktop, faDatabase]);
