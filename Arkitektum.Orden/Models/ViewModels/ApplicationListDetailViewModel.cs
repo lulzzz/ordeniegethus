885fee460a0a3eb407caef94d1dba1d1ce4e8a3b
Anastasiya Bukhala <anastasiya@arkitektum.no>
2018-04-19 15:15:22 +0200

    View models for filtering of applications by sector

---
+++ Arkitektum.Orden/Models/ViewModels/ApplicationListDetailViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arkitektum.Orden.Models.ViewModels
+{
+    public class ApplicationListDetailViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string AnnualFee { get; set; }
+        public string SystemOwner { get; set; }
+        public string Vendor { get; set; }
+
+        public IEnumerable<ApplicationListDetailViewModel> Map(IEnumerable<Application> applications)
+        {
+            var model = new List<ApplicationListDetailViewModel>();
+            foreach (var application in applications)
+            {
+                model.Add(Map(application));
+            }
+
+            return model;
+        }
+
+        private ApplicationListDetailViewModel Map(Application input)
+        {
+            return new ApplicationListDetailViewModel
+            {
+                Id = input.Id,
+                AnnualFee = input.DecimalToString(input.AnnualFee),
+                Name = input.Name,
+                Version = input.Version,
+                SystemOwner = input.SystemOwner?.FullName,
+                Vendor = input.Vendor?.Name,
+            };
+        }
+    }
+}
---
+++ Arkitektum.Orden/Models/ViewModels/ApplicationListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Arkitektum.Orden.Models.ViewModels
+{
+    public class ApplicationListViewModel
+    {
+        public IEnumerable<ApplicationListDetailViewModel> Applications { get; set; }
+        public List<SelectListItem> Sectors { get; set; } 
+
+    }
+}
