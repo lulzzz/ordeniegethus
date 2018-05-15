c1593c2c517cb9247be94915a81d8e5f03e2c014
Benjamin Dehli <benjamin@arkitektum.no>
2018-05-15 11:27:59 +0200

    Adjust layout GUI

--- Arkitektum.Orden/ClientApp/components/views/Application.vue
+++ Arkitektum.Orden/ClientApp/components/views/Application.vue
@@ -7,17 +7,17 @@
 			</div>
 			<div class="col-md-8">
 				<h1>{{ apiData.name }}  - versjon {{ apiData.version }}</h1>
-                
+				
 				<div class="btn-group" v-if="writeAccess">
 					<a v-bind:href="apiUrls.edit" class="btn btn-sm btn-outline-primary"><i class="fas fa-edit"></i> Rediger</a>
 					<a v-bind:href="apiUrls.submitAppRegistry" class="btn btn-sm btn-outline-primary"><i class="fas fa-warehouse"></i> Send inn til felles applikasjonsregister</a>
 				</div>
 				
-                <div id="details" class="section">
-                    <p v-if="apiData.userModified">Sist endret av {{ apiData.userModified }} - {{ apiData.dateModified | formatDateTime }}</p>
-                    <p v-if="apiData.userCreated">Opprettet av: {{ apiData.userCreated }} - {{ apiData.dateCreated | formatDateTime }}</p>
-                    <p>Produsent: {{ apiData.vendor.name }}</p>
-                    <p v-if="apiData.systemOwnerName">Systemansvarlig: <span class="badge badge-info"><i class="fas fa-user"></i> {{ apiData.systemOwnerName }}</span></p>
+				<div id="details" class="section">
+					<p v-if="apiData.userModified">Sist endret av {{ apiData.userModified }} - {{ apiData.dateModified | formatDateTime }}</p>
+					<p v-if="apiData.userCreated">Opprettet av: {{ apiData.userCreated }} - {{ apiData.dateCreated | formatDateTime }}</p>
+					<p>Produsent: {{ apiData.vendor.name }}</p>
+					<p v-if="apiData.systemOwnerName">Systemansvarlig: <span class="badge badge-info"><i class="fas fa-user"></i> {{ apiData.systemOwnerName }}</span></p>
 				</div>
 
 				<div id="cost" class="section" v-if="hasCostDetails()">
@@ -27,11 +27,11 @@
 					<p v-if="apiData.purchaseDate">Anskaffelsesdato: {{ apiData.purchaseDate | formatDate }}</p>
 				</div>
 				
-                <div id="hosting" class="section">
-                    <h2>Drift</h2>
-                    <p>Driftsleverandør: {{ apiData.hostingVendor }}</p>
-                    <p>Driftsplassering: {{ apiData.hostingLocationText }}</p>
-                    <p>Antall brukere: {{ apiData.numberOfUsers }}</p>
+				<div id="hosting" class="section">
+					<h2>Drift</h2>
+					<p>Driftsleverandør: {{ apiData.hostingVendor }}</p>
+					<p>Driftsplassering: {{ apiData.hostingLocationText }}</p>
+					<p>Antall brukere: {{ apiData.numberOfUsers }}</p>
 				</div>
 				
 				<div id="agreement" class="section" v-if="hasAgreementDetails()">
@@ -43,35 +43,35 @@
 					<p v-if="apiData.agreementDocumentUrl"><a v-bind:href="apiData.agreementDocumentUrl">Lenke til avtale</a></p>
 				</div>
 
-                <div id="super-users" class="section">
-				    <h2>Superbrukere</h2>
-				    <application-super-users v-bind:organization-id="organizationId" v-bind:application-id="applicationId" route="/api/applications" v-bind:write-access="writeAccess"></application-super-users>
+				<div id="super-users" class="section">
+					<h2>Superbrukere</h2>
+					<application-super-users v-bind:organization-id="organizationId" v-bind:application-id="applicationId" route="/api/applications" v-bind:write-access="writeAccess"></application-super-users>
 				</div>
 
-                <div id="resource-links" class="section">
-				    <h2>Lenker</h2>
-				    <resource-links v-bind:parent-id="applicationId" api-path="/ResourceLinks/Application" v-bind:write-access="writeAccess"></resource-links>
+				<div id="resource-links" class="section">
+					<h2>Lenker</h2>
+					<resource-links v-bind:parent-id="applicationId" api-path="/ResourceLinks/Application" v-bind:write-access="writeAccess"></resource-links>
 				</div>
 
-                <div class="section">
-				    <h2 id="sectors">Tjenesteområder</h2>
-				    <application-sectors v-bind:application-sectors="apiData.sectors" v-bind:applicationId="applicationId" v-bind:write-access="writeAccess"></application-sectors>
+				<div class="section">
+					<h2 id="sectors">Tjenesteområder</h2>
+					<application-sectors v-bind:application-sectors="apiData.sectors" v-bind:applicationId="applicationId" v-bind:write-access="writeAccess"></application-sectors>
 				</div>
 
-                <div id="dataset" class="section">
-				    <h2>Datasett</h2>
-				    <application-datasets v-bind:application-datasets="apiData.datasets" v-bind:applicationId="applicationId" v-bind:write-access="writeAccess"></application-datasets>
-                </div>
-				
-                <div id="national-components" class="section">
-				    <h2>Nasjonale felleskomponenter</h2>    
-				    <application-national-components v-bind:application-national-components="apiData.nationalComponents" v-bind:applicationId="applicationId" v-bind:write-access="writeAccess"></application-national-components>
+				<div id="dataset" class="section">
+					<h2>Datasett</h2>
+					<application-datasets v-bind:application-datasets="apiData.datasets" v-bind:applicationId="applicationId" v-bind:write-access="writeAccess"></application-datasets>
 				</div>
 				
-                <div id="standards" class="section">
-				    <h2>Standarder</h2>
-				    <application-standards v-bind:application-standards="apiData.standards" v-bind:applicationId="applicationId" v-bind:write-access="writeAccess"></application-standards>
-                </div>
+				<div id="national-components" class="section">
+					<h2>Nasjonale felleskomponenter</h2>    
+					<application-national-components v-bind:application-national-components="apiData.nationalComponents" v-bind:applicationId="applicationId" v-bind:write-access="writeAccess"></application-national-components>
+				</div>
+				
+				<div id="standards" class="section">
+					<h2>Standarder</h2>
+					<application-standards v-bind:application-standards="apiData.standards" v-bind:applicationId="applicationId" v-bind:write-access="writeAccess"></application-standards>
+				</div>
 
 			</div>
 		</div>
--- Arkitektum.Orden/ClientApp/components/views/Dataset.vue
+++ Arkitektum.Orden/ClientApp/components/views/Dataset.vue
@@ -1,85 +1,85 @@
 <template>
-  <div v-if="apiData">
-   
-    <div class="row">
-      <div class="col-md-3 border-right">
-        <sidebar-navigation 
-        v-bind:navigation-items="navigationItems"></sidebar-navigation>
-      </div>
-      <div class="col-md-9">
-         <h1>{{ apiData.name }}</h1>
-        <div class="btn-group" v-if="writeAccess">
-          <a v-bind:href="apiUrls.edit" class="btn btn-sm btn-outline-primary">
-            <i class="fas fa-edit"></i>
-            Rediger
-          </a>
-        </div>
+	<div v-if="apiData">
+		
+		<div class="row">
+			<div class="col-md-3 border-right">
+				<sidebar-navigation 
+				v-bind:navigation-items="navigationItems"></sidebar-navigation>
+			</div>
+			<div class="col-md-9">
+				<h1>{{ apiData.name }}</h1>
+				<div class="btn-group" v-if="writeAccess">
+					<a v-bind:href="apiUrls.edit" class="btn btn-sm btn-outline-primary">
+						<i class="fas fa-edit"></i>
+						Rediger
+					</a>
+				</div>
 
-        <div id="details" class="section">
-          <p v-if="apiData.userModified">
-            Sist endret av {{ apiData.userModified }} - {{ apiData.dateModified |
-            formatDate }}
-          </p>
-          <p v-if="apiData.userCreated">
-            Opprettet av: {{ apiData.userCreated }} - {{ apiData.dateCreated |
-            formatDate }}
-          </p>
+				<div id="details" class="section">
+					<p v-if="apiData.userModified">
+						Sist endret av {{ apiData.userModified }} - {{ apiData.dateModified |
+						formatDate }}
+					</p>
+					<p v-if="apiData.userCreated">
+						Opprettet av: {{ apiData.userCreated }} - {{ apiData.dateCreated |
+						formatDate }}
+					</p>
 
-          <p>
-            Inneholder personopplysninger: <strong>{{ apiData.hasPersonalData | formatBoolean }}</strong>
-          </p>
-          <p>
-            Inneholder sensitive personopplysninger: <strong>{{ apiData.hasSensitivePersonalData | formatBoolean }}</strong>
-          </p>
-          <p>
-            Inneholder master data: <strong>{{ apiData.hasMasterData | formatBoolean }}</strong>
-          </p>
-        </div>
+					<p>
+						Inneholder personopplysninger: <strong>{{ apiData.hasPersonalData | formatBoolean }}</strong>
+					</p>
+					<p>
+						Inneholder sensitive personopplysninger: <strong>{{ apiData.hasSensitivePersonalData | formatBoolean }}</strong>
+					</p>
+					<p>
+						Inneholder master data: <strong>{{ apiData.hasMasterData | formatBoolean }}</strong>
+					</p>
+				</div>
 
-        <div id="description" class="section">
-          <h2>Beskrivelse</h2>
-          <p v-if="apiData.description" class="long-text">{{ apiData.description }}</p>
-          <p v-if="!apiData.description">
-            <i>Ingen beskrivelse angitt.</i>
-          </p>
-        </div>
-  
-        <div id="purpose" class="section">
-          <h2>Formål</h2>
-          <p v-if="apiData.purpose" class="long-text">{{ apiData.purpose }}</p>
-          <p v-if="!apiData.purpose">
-            <i>Ingen formål angitt.</i>
-          </p>
-        </div>
-       
-        <div id="hosting" class="section">
-          <h2>Drift</h2>
-          <p v-if="apiData.hostingLocationText">Driftsplassering: {{ apiData.hostingLocationText }}</p>
-        </div>
+				<div id="description" class="section">
+					<h2>Beskrivelse</h2>
+					<p v-if="apiData.description" class="long-text">{{ apiData.description }}</p>
+					<p v-if="!apiData.description">
+						<i>Ingen beskrivelse angitt.</i>
+					</p>
+				</div>
+				
+				<div id="purpose" class="section">
+					<h2>Formål</h2>
+					<p v-if="apiData.purpose" class="long-text">{{ apiData.purpose }}</p>
+					<p v-if="!apiData.purpose">
+						<i>Ingen formål angitt.</i>
+					</p>
+				</div>
+				
+				<div id="hosting" class="section">
+					<h2>Drift</h2>
+					<p v-if="apiData.hostingLocationText">Driftsplassering: {{ apiData.hostingLocationText }}</p>
+				</div>
 
-        <div id="details" class="section">
-          <h2 id="fields">Informasjonselementer</h2>
-          <dataset-fields v-bind:dataset-id="datasetId" v-bind:dataset-fields="apiData.fields" v-bind:write-access="writeAccess"></dataset-fields>
-        </div>
+				<div id="details" class="section">
+					<h2 id="fields">Informasjonselementer</h2>
+					<dataset-fields v-bind:dataset-id="datasetId" v-bind:dataset-fields="apiData.fields" v-bind:write-access="writeAccess"></dataset-fields>
+				</div>
 
-        <div id="details" class="section">
-          <h2 id="metadata">Metadata</h2>
-          <dataset-metadata v-bind:write-access="writeAccess" v-bind:dataset-id="datasetId" v-bind:lists="metadataLists"></dataset-metadata>
-        </div>
+				<div id="details" class="section">
+					<h2 id="metadata">Metadata</h2>
+					<dataset-metadata v-bind:write-access="writeAccess" v-bind:dataset-id="datasetId" v-bind:lists="metadataLists"></dataset-metadata>
+				</div>
 
-        <div id="resource-links" class="section">
-          <h2>Lenker</h2>
-          <resource-links v-bind:parent-id="datasetId" api-path="/api/ResourceLinks/Dataset" v-bind:write-access="writeAccess"></resource-links>
-        </div>
+				<div id="resource-links" class="section">
+					<h2>Lenker</h2>
+					<resource-links v-bind:parent-id="datasetId" api-path="/api/ResourceLinks/Dataset" v-bind:write-access="writeAccess"></resource-links>
+				</div>
 
-        <div id="applications">
-          <h2>Applikasjoner</h2>
-          <dataset-applications v-bind:dataset-id="datasetId" v-bind:dataset-applications="apiData.applications" v-bind:write-access="writeAccess"></dataset-applications>
-        </div>
+				<div id="applications" class="section">
+					<h2>Applikasjoner</h2>
+					<dataset-applications v-bind:dataset-id="datasetId" v-bind:dataset-applications="apiData.applications" v-bind:write-access="writeAccess"></dataset-applications>
+				</div>
 
-      </div>
-    </div>
-  </div>
+			</div>
+		</div>
+	</div>
 
 </template>
 
